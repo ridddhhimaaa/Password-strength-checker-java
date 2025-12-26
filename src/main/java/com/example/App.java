4f90a88abdac22b984c2fb2fa91a90ea77606446
ridddhhimaaa <riddhimarai1704@gmail.com>
2025-12-26 13:42:55 +0530

    Added Add Student feature

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,8 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Student Management System");
+        System.out.println("Feature: Add Student Module");
     }
 }
